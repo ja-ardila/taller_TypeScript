dc72a076c6c0aff00c7b4930c846f6864de4a937
Juan Andrés Ardila <ja.ardila2@uniandes.edu.co>
2023-10-23 05:02:34 +0000

    Tercer avance proyecto

---
+++ scripts/curso.js
@@ -0,0 +1,10 @@
+var Curso = /** @class */ (function () {
+    function Curso(nombre, horas, calificacion, certificado, anio) {
+        this.nombre = nombre;
+        this.horas = horas;
+        this.calificacion = calificacion;
+        this.certificado = certificado;
+    }
+    return Curso;
+}());
+export { Curso };
